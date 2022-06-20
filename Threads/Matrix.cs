f006ebf1b9ceb4df341efac6a63461bfbff0a1dd
Dominika Arkabus <252839@student.pwr.edu.pl>
2022-06-20 22:52:09 +0200

    Dodaj pliki projektów.

---
+++ Threads/Matrix.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+   
+    public class Matrix
+    {
+        int SIZE = 100;
+        private readonly object _lock;
+        public List<Kolumny> wiersze;
+        public List<int> max_num;
+        public List<int> max_num_without_threats;
+
+
+        public Matrix()
+        {
+            wiersze = new List<Kolumny>(SIZE);
+            max_num = new List<int>();
+            max_num_without_threats = new List<int>();
+            _lock = new object();
+            fill();
+        }
+
+        public void fill()
+        {
+
+            Random random = new Random();
+            Console.WriteLine("tworze macierz");
+            for (int i = 0; i < SIZE; i++)
+            {
+                //Random random = new Random(i);
+                Kolumny a = new Kolumny(random);
+                wiersze.Add(a);
+            }
+            Console.WriteLine("stworzylem macierz");
+
+        }
+
+        public void max_search()
+        {
+            for(int i = 0; i < wiersze.Count; i++)
+            {
+                bool choice = false;
+                lock (_lock)
+                {
+                    if (!wiersze[i].ifChecked)
+                    {
+                        choice = true;
+                        wiersze[i].ifChecked = true;
+                    }
+                }
+                if (wiersze[i].ifChecked && choice)
+                {
+                    max_num.Add(wiersze[i].maxx());
+                }
+            }
+                
+        }
+        public void max_search_without_threats()
+        {
+            for (int i = 0; i < wiersze.Count; i++)
+            {
+                max_num_without_threats.Add(wiersze[i].maxx());
+            }
+        }
+
+        public int max_max_search()
+        {
+            return max_num.Max();
+        }
+
+        public int max_max_search_without_threats()
+        {
+            return max_num_without_threats.Max();
+        }
+
+
+        public void print_max()
+
+        {
+            Console.Write("\n");
+            for (int i = 0; i < max_num.Count; i++)
+            {
+                Console.Write(max_num[i].ToString() + "\t");
+            }
+            Console.Write("\n");
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Kolumny i in wiersze)
+            {
+                str += i.ToString() + "\n";
+            }
+            return str;
+        }
+
+
+
+    }
+
+
+    public class Kolumny
+    {
+        int SIZE = 100;
+        List<int> kolumny;
+        public bool ifChecked;
+
+
+
+        public Kolumny(Random random)
+        {
+            kolumny = new List<int>(SIZE);
+            ifChecked = false;
+            fill(random);
+        }
+
+        public void fill(Random random)
+        {
+            for (int i = 0; i <SIZE; i++)
+            {
+                kolumny.Add(random.Next()%32000);
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach(int i in kolumny)
+            {
+                str += i.ToString() + "\t";
+            }
+            return str;
+        }
+
+        public int maxx()
+        {
+            return kolumny.Max();
+        }
+
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Threads
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Matrix macierz = new Matrix();
+            //Console.Write(macierz.ToString());
+
+            Stopwatch sw = new Stopwatch();
+            Stopwatch sw1 = new Stopwatch();
+            Thread[] threads = new Thread[3];
+            sw.Start();
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(macierz.max_search);
+                threads[i].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine ("watki zakonczyly prace");
+            //macierz.print_max();
+
+            Console.WriteLine("max ze wszystkich liczb wynosi: " + macierz.max_max_search().ToString());
+            sw.Stop();
+            Console.WriteLine("czas pracy z wątkami wynosi: " + sw.Elapsed.ToString());
+
+            sw1.Start();
+            macierz.max_search_without_threats();
+            Console.WriteLine("max ze wszystkich liczb bez wątków wynosi : " + macierz.max_max_search_without_threats());
+            sw1.Stop();
+            Console.WriteLine("czas pracy bez wątków wynosi : " + sw1.Elapsed.ToString());
+
+            Console.ReadLine();
+        }
+
+    }
+}
